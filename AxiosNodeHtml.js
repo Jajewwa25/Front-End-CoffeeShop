5065478c3c76d52f4665b92db96d16c9622f2390
Kotchapun Roekcharoen <kxttpw.j@gmail.com>
2024-03-01 14:29:47 +0700

    save

--- AxiosNodeHtml.js
+++ AxiosNodeHtml.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const axios = require("axios");
 const app = express();
+const path = require("path");
 var bodyParser = require("body-parser");
 
-const base_url = "http://node59553-env-5865143.proen.app.ruk-com.cloud";
+const base_url = "http://localhost:3000";
 //"body-parser": "^1.20.2",
 
 app.set("view engine", "ejs");
@@ -12,15 +13,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(__dirname + "/public"));
 
-/*app.get("/", async (req, res) => {
-  try {
-      const response = await axios.get(base_url + "/product/" + req.params.id);
-      res.render("menu", { shop: product.data});
-  } catch (err) {
-      console.error(err);
-      res.status(500).send('Error menu')
-  }
-});*/
+app.get("/", async (req, res) => {
+
+      const response = await axios.get(base_url + "/Customers");
+      res.render("login", {Customer : response.data});
+  
+});
 
 app.listen(5500, () => {
   console.log("server started on port 5500");
